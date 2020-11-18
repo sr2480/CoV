f64b3453fe2952dd8ce0f1bef054c68efb86e419
Samantha Ramirez <s.ramirez@oceans.ubc.ca>
2020-11-18 11:29:00 -0800

    Testthat.R changes

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,9 @@
 library(testthat)
 library(CoV)
 
-test_check("CoV")
+test_that("CoV", {
+  expect_lt(my_function(b), 1000)
+  expect_type(my_function(b), "double")
+  expect_message(my_function(b),"calculating mean")
+  expect_message(my_function(b),"calculating standard deviation")
+  expect_message(my_function(b),"calculating the coefficient of variance")
